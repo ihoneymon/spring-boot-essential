0667f98e8c11bea5bd5e5db48af5b54740554eb1
Honeymon <ihoneymon@gmail.com>
2022-03-25 23:06:44 +0900

    Init Project: Spring Boot Essential 싱글모듈 프로젝트 설정

---
+++ src/main/java/com/fastcampus/sr/sbessential/SpringBootEssentialApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.sr.sbessential;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEssentialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEssentialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fastcampus/sr/sbessential/SpringBootEssentialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.sr.sbessential;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEssentialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
